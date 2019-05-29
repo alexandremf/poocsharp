ac5a59108d20765ea2fbf7f8418e6f0fd6b043f7
alexandremf <ferreiramgbr@gmail.com>
2019-05-29 11:31:05 -0300

    Aprendendo Poo Com Canetas

---
+++ Caneta/Caneta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanetaBic.Caneta
+{
+    class Caneta
+    {
+        private string modelo;
+        public void SetModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public string GetModelo()
+        {
+            return modelo;
+        }
+
+
+
+        public string cor { get; set; }
+        public float ponta { get; set; }
+
+
+
+        private bool carga;
+        public void SetCarga(bool carga)
+        {
+            this.carga = carga;
+        }
+        public bool GetCarga()
+        {
+            return carga;
+        }
+
+
+        public bool tampada { get; set; }
+
+        public bool TamparCaneta()
+        {
+            if (tampada == true)
+            {
+                Console.WriteLine("A caneta está tampada!");
+            }
+            else
+            {
+                Console.WriteLine("A caneta está destampada");
+            }
+
+            return tampada;
+        }
+
+
+
+
+
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanetaBic.Caneta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caneta caneta = new Caneta();
+
+            caneta.SetModelo("BIC");
+            Console.WriteLine("Modelo da Caneta " + caneta.GetModelo());
+
+            caneta.tampada = true;
+            Console.WriteLine("A Caneta está tampada? " + caneta.tampada);
+
+            caneta.cor = "Azul";
+            Console.WriteLine("Cor da Caneta: " + caneta.cor);
+
+            caneta.SetCarga(true);
+            Console.WriteLine("Carga da Caneta " + caneta.GetCarga());
+
+
+            Console.ReadLine();
+
+           
+        }
+    }
+}
